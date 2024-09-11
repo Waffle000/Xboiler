43f0f14e8e087c3ac0b34a2bdb0df450008444ca
Waffle00 <enrico.maricar@gmail.com>
2024-09-11 21:37:43 +0700

    chore : fixing versioning check

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,21 +5,50 @@ import Foundation
 import ArgumentParser
 
 struct Generate: ParsableCommand {
-    @Argument(help: "Path ke file YAML.")
+    @Argument(help: "Access Path YAML.")
     var yamlFilePath: String
     
     func run() throws {
-        if let yamlData = parseYAML(filePath: yamlFilePath) {
-            ProjectGenerator().generateProject(from: yamlData)
-        } else {
-            print("Gagal mem-parsing YAML.")
+        do {
+            if let yamlData = parseYAML(filePath: yamlFilePath) {
+                ProjectGenerator().generateProject(from: yamlData)
+            } else {
+                throw ValidationError("Failed read YAML")
+            }
+        } catch {
+            print("Error: \(error.localizedDescription)")
+            throw error
         }
     }
 }
 
+func fetchGitHubTag() -> String {
+    let url = URL(string: "https://api.github.com/repos/Waffle000/Xboiler/tags")!
+    var version = "Unknown"
+    let semaphore = DispatchSemaphore(value: 0)
+
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data {
+            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]],
+               let latestTag = json.first?["name"] as? String {
+                version = latestTag
+            }
+        }
+        semaphore.signal()
+    }
+    task.resume()
+    semaphore.wait()
+    return version
+}
+
 struct XBoiler: ParsableCommand {
+    static var version: String {
+        return fetchGitHubTag()
+    }
+
     static let configuration = CommandConfiguration(
         abstract: "XBoiler - Generate SwiftUI boilerplate from YAML",
+        version: version,
         subcommands: [Generate.self],
         defaultSubcommand: Generate.self
     )
